aeea47af292630d046132b9e03b601e84e3cfd56
GustavoW <guvieirawalter@gmail.com>
2025-07-22 18:51:21 -0300

    Adicionada pasta ORM no WebApp, arrumada integração com o bootstrap, adicionado ActionFilter de ValidarModeloAttribute, program reajustado

--- GeradorDeTestes.WebApp/Program.cs
+++ GeradorDeTestes.WebApp/Program.cs
@@ -1,9 +1,14 @@
 using GeradorDeTestes.Dominio.ModuloDisciplina;
 using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Dominio.ModuloQuestao;
 using GeradorDeTestes.Dominio.ModuloTeste;
 using GeradorDeTestes.Infraestrutura.Orm.ModuloDisciplina;
 using GeradorDeTestes.Infraestrutura.Orm.ModuloMateria;
+using GeradorDeTestes.Infraestrutura.Orm.ModuloQuestao;
 using GeradorDeTestes.Infraestrutura.Orm.ModuloTeste;
+using GeradorDeTestes.WebApp.ActionFilters;
+using GeradorDeTestes.WebApp.DependencyInjection;
+using GeradorDeTestes.WebApp.Orm;
 
 namespace GeradorDeTestes.WebApp;
 
@@ -13,19 +18,33 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddScoped<IRepositorioDisciplina, RepositorioDisciplinaEmOrm>();
-        builder.Services.AddScoped<IRepositorioMateria, RepositorioMateriaEmOrm>();
-        builder.Services.AddScoped<IRepositorioTeste, RepositorioTesteEmOrm>();
-        
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddScoped<IRepositorioDisciplina, RepositorioDisciplinaEmOrm>();
+            builder.Services.AddScoped<IRepositorioMateria, RepositorioMateriaEmOrm>();
+            builder.Services.AddScoped<IRepositorioTeste, RepositorioTesteEmOrm>();
+            builder.Services.AddScoped<IRepositorioQuestao, RepositorioQuestaoEmOrm>();
+            builder.Services.AddEntityFrameworkConfig(builder.Configuration);
+        }
 
-        builder.Services.AddEntityFrameworkConfig(builder.Configuration);
-        builder.Services.AddControllersWithViews();
+        builder.Services.AddSerilogConfig(builder.Logging);
+
+        builder.Services.AddControllersWithViews(options =>
+        {
+            options.Filters.Add<ValidarModeloAttribute>();
+        });
 
         var app = builder.Build();
 
-        app.UseAntiforgery();
+        if (app.Environment.IsDevelopment())
+            app.ApplyMigrations();
+
+        builder.Services.AddEntityFrameworkConfig(builder.Configuration);
+
+            app.UseAntiforgery();
         app.UseStaticFiles();
         app.UseRouting();
+
         app.MapDefaultControllerRoute();
 
         app.Run();
